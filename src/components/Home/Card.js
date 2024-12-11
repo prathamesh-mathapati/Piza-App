bb36cd565fdbb5c6678b665d977df1f9e27d7ceb
prathamesh-mathapatik <prathamesh20@navgurukul.org>
2024-12-11 17:48:19 +0530

    Called card on dynamically

--- src/components/Home/Card.js
+++ src/components/Home/Card.js
@@ -1,8 +1,9 @@
 import React from "react";
 import Image from "next/image";
 
-const Card = () => {
-  const priceOptions = ["medium", "regular", "large"];
+const Card = (props) => {
+  const {foodData}=props
+
   
   return (
     <div className="box">
@@ -12,32 +13,24 @@ const Card = () => {
             layout="fill"
             objectFit="cover"
             alt="Farmhouse pizza"
-            src="https://images.dominos.co.in/nextgen-catalog/media/prod/Dominos/WebHomeProductV1/d6ef0e48-39e8-4b1f-908a-284f3ffd4808_farmhouse_side.webp?ver=V0.0.1"
+            src={foodData.img}
           />
         </div>
         <div className="p-4">
-          <div className="font-bold mb-2 text-xl uppercase">Pizza</div>
+          <div className="font-bold mb-2 text-xl uppercase">{foodData?.category}</div>
           <p className="short_description text-gray-700 dark:text-gray-400 text-base">
-            Lorem Ipsum is simply dummy text of the printing and typesetting
-            industry. Lorem Ipsum has been the industry's standard dummy text
-            ever since the 1500s, when an unknown printer took a galley of type
-            and scrambled it to make a type specimen book. It has survived not
-            only five centuries, but also the leap into electronic typesetting,
-            remaining essentially unchanged. It was popularised in the 1960s
-            with the release of Letraset sheets containing Lorem Ipsum passages,
-            and more recently with desktop publishing software like Aldus
-            PageMaker including versions of Lorem Ipsum.
+            {foodData?.description}
           </p>
         </div>
         <div className="flex px-4 justify-between">
-          <select>
+          <select  className=" h-100  p-1 text-black hover:font-bold font-semibold cursor-pointer dark:text-gray-300  border border-black dark:border-gray-400 rounded">
             {Array.from(Array(6), (e, i) => {
               return <option key={i + 1}>{1 + i}</option>;
             })}
           </select>
 
-          <select>
-            {priceOptions.map((item, index) => {
+          <select  className=" h-100  p-1 text-black hover:font-bold font-semibold cursor-pointer dark:text-gray-300  border border-black dark:border-gray-400 rounded">
+            {Object.keys(foodData.price).map((item, index) => {
               return <option key={index}>{item}</option>;
             })}
           </select>
