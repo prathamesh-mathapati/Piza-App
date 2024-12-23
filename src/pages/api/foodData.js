89b37b33b8bcfab3432af5fd1368f1058c29b523
prathamesh-mathapatik <prathamesh20@navgurukul.org>
2024-12-23 17:36:28 +0530

    Make  pizza schema

---
+++ src/pages/api/foodData.js
@@ -0,0 +1,21 @@
+import db from "@/utlis/db";
+import PizzData from "../models/Pizzadata";
+
+export default async function  handler (req, res) {
+    if(req.methode==="POST"){
+        await db.connect();
+        for(let i=0;i< req.body.length; i++){
+            let pizza=new PizzData({
+                name:req.body[i].name,
+                category:req.body[i].category,
+                  foodType: req.body[i].foodType,
+                  price: req.body[i].price,
+                  description: req.body[i].description,
+                  img: req.body[i].img
+            })
+            await pizza.save();
+        }
+        res.status(200).json({ message: "all data is send" });
+    }
+
+}
